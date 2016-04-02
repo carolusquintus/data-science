aee7c6542368cff490e88e5015edae3939858f68
Carlos Rosas <carlos.rosas@2hsoftware.com.mx>
2016-04-01 18:57:17 -0600

    Update of week 2

---
+++ R programming/week2/pollutantmean.R
@@ -0,0 +1,55 @@
+sysname <- Sys.info()["sysname"]
+
+if (identical(sysname, "Windows"))
+	paste0(Sys.getenv("USERPROFILE"), "/Desarrollo/Coursera/Data Science/R programming/week2")
+
+pollutantmean <- function(directory, pollutant = "sulfate", id = 1:332) {
+	## 'directory' is a character vector of length 1 indicating
+	## the location of the CSV files
+
+	## 'pollutant' is a character vector of length 1 indicating
+	## the name of the pollutant for which we will calculate the
+	## mean; either "sulfate" or "nitrate"
+
+	## 'id' is an integer vector indicating the monitor ID numbers
+	## to be used
+
+	## Return the mean of the pollutant across all monitors list
+	## int the 'id' vector (ignoring NA values)
+	## NOTE: Do not round the result
+
+	oneZero <- "0"
+	douZero <- "00"
+	csvExtension <- ".csv"
+
+	csvFile <- ""
+	totalPollutant <- c()
+
+	for (idFile in id) {
+		if (idFile >= 1 && idFile <= 9)
+			csvFile <- paste0(douZero, idFile, csvExtension)
+		else if (idFile >= 10 && idFile <= 99)
+			csvFile <- paste0(oneZero, idFile, csvExtension)
+		else
+			csvFile <- paste0(idFile, csvExtension)
+
+		data <- read.csv(paste(directory, csvFile, sep = "/"))
+
+		# Option 1
+		#dataPollutant <- data[!is.na(data[, pollutant]), pollutant]
+		dataPollutant <- na.omit(data[, pollutant])
+		# Option 2 with NA
+		dataPollutant <- data[[pollutant]]
+
+		totalPollutant <- c(totalPollutant, dataPollutant)
+	}
+
+	# Option 1
+	mean(totalPollutant)
+	# Option 2
+	mean(totalPollutant, na.rm = TRUE)
+}
+
+print(pollutantmean("specdata", "sulfate", 1:10))
+print(pollutantmean("specdata", "nitrate", 70:72))
+print(pollutantmean("specdata", "nitrate", 23))
