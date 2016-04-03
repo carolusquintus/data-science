34b7662a087b5b9c236355d28a742cc9be862584
Carlos Rosas <carloschars@gmail.com>
2016-04-03 14:12:52 -0500

    complete.R script finished

--- R programming/week2/complete.R
+++ R programming/week2/complete.R
@@ -17,4 +17,36 @@ complete <- function(directory, id = 1:332) {
 	## ...
 	## where 'id' is the monitor ID number and 'nobs' is the
 	## number of complete cases
+
+	oneZero <- "0"
+	douZero <- "00"
+	csvExtension <- ".csv"
+
+	csvFile <- ""
+	complete <- data.frame(numeric(), numeric())
+
+	for (j in seq_along(id)) {
+		#print(j)
+		#print(id[j])
+		if (id[j] >= 1 && id[j] <= 9)
+			csvFile <- paste0(douZero, id[j], csvExtension)
+		else if (id[j] >= 10 && id[j] <= 99)
+			csvFile <- paste0(oneZero, id[j], csvExtension)
+		else
+			csvFile <- paste0(id[j], csvExtension)
+
+		data <- read.csv(paste(directory, csvFile, sep = "/"))
+		nrow <- nrow(data[complete.cases(data),])
+
+		#print(nrow)
+
+		complete <- rbind(complete, c(id[j], nrow))
+	}
+	colnames(complete) <- c("id", "nobs")
+	complete
 }
+
+print(complete("specdata", 1))
+print(complete("specdata", c(2, 4, 8, 10, 12)))
+print(complete("specdata", 30:25))
+print(complete("specdata", 3))
